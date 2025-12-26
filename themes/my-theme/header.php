6a06a4a1650af03df71a6e29a87b91456106e6d2
nimol <limnimol.dev@gmail.com>
2025-12-26 10:35:42 +0700

    done hero banner

--- themes/my-theme/header.php
+++ themes/my-theme/header.php
@@ -5,11 +5,16 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
-        <?php wp_nav_menu(['theme_location'=>'primary', 
-        'menu_class'     => 'menu',
         
-    ]); ?>
-    
+        <div class="logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/logo.png" alt="<?php bloginfo('clinic'); ?>">
+        </div>
+        <nav class="menu-items">
+            <?php wp_nav_menu(['theme_location'=>'primary', 
+            'menu_class'     => 'menu',
+        
+            ]); ?>
+        </nav>
     </header>
 
     
