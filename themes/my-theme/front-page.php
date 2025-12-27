012ad50179af239bebe10fdef67a8a9295a72dc8
nimol <limnimol.dev@gmail.com>
2025-12-27 16:34:05 +0700

    home page 80%

--- themes/my-theme/front-page.php
+++ themes/my-theme/front-page.php
@@ -64,18 +64,17 @@
 </section>
  
 <section class="doctor-section">
-    <div class="doctors-content">
         <h1 class="tittle"><span>Our Expert</span> Doctors</h1>
         
         <div class="doctor-list">
+            
             <div class="doctor-box">
-                
                 <div class="image">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/image/doctor.png" alt="<?php bloginfo('clinic'); ?>">
                 </div>
                 
-                <div class="infor-dr">
-                    <h2 class="name">Dr. John Doe</h2>
+                <div class="name-dr">
+                    <h3 class="name">Dr. Jane Smith </h3>
                     <p>Department</p>
                     
                     <div class="icon-dr">
@@ -83,14 +82,161 @@
                         <i class="fab fa-facebook"></i>
                         <i class="fab fa-instagram"></i>
                     </div>
-                        
+                    <div class="btn-pf">
+                        <button class="profile">View Profile</button>
+                    </div>
                 </div>
-               
             </div>
+
+            <div class="doctor-box">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/image/doctor1.png" alt="<?php bloginfo('clinic'); ?>">
+                </div>
+                
+                <div class="name-dr">
+                    <h3 class="name">Dr. Marry Lou</h3>
+                    <p>Department</p>
+                    
+                    <div class="icon-dr">
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="btn-pf">
+                        <button class="profile">View Profile</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="doctor-box">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/image/doctor2.png" alt="<?php bloginfo('clinic'); ?>">
+                </div>
+                
+                <div class="name-dr">
+                    <h3 class="name">Dr. Sansa Stark</h3>
+                    <p>Department</p>
+                    
+                    <div class="icon-dr">
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="btn-pf">
+                        <button class="profile">View Profile</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="doctor-box">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/image/doctor3.png" alt="<?php bloginfo('clinic'); ?>">
+                </div>
+                
+                <div class="name-dr">
+                    <h3 class="name">Dr. John Doe</h3>
+                    <p>Department</p>
+                    
+                    <div class="icon-dr">
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="btn-pf">
+                        <button class="profile">View Profile</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     
-    </div>
+    </div>   
+</section>
 
+<section class="svervice-page">
+    <h1 class="tittle"><span>Our Provided</span> Pervices</h1>
+    
+        <div class="service-content">
+            
+            <div class="service-item">
+                
+                <div class="service-box">
+                    
+                        <i class="fas fa-heartbeat"></i>
+                    <h3>Cardiology</h3>
+                    <p>Comprehensive cardiac care including diagnostics, treatment, and rehabilitation for heart conditions conditions.</p>
+
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                    </div>
+
+                </div>
+
+                <div class="service-box">
+                        <i class="fa-solid fa-baby"></i>
+                    <h3>Pediatrics</h3>
+                    <p>Dedicated healthcare services for infants, children, and adolescents to ensure healthy growth and development.</p>
+
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                    </div>
+
+                </div>
+
+                <div class="service-box">
+                        <i class="fa-solid fa-wheelchair"></i>
+                    <h3>Orthopedics</h3>
+                    <p>Advanced treatment for musculoskeletal issues including fractures, joint problems, and sports injuries.</p>
+
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                </div>
+
+                </div>
+
+                
+
+            </div>
+        
+            <div class="service-item">
+                
+                <div class="service-box">
+                        <i class="fa-solid fa-brain"></i>
+                    <h3>Neurology</h3>
+                    <p>Expert care for neurological disorders such as epilepsy, stroke, and neurodegenerative diseases.</p>
+
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                    </div>
+                </div>
+                
+                <div class="service-box">
+                       <i class="fas fa-viruses"></i>
+                    <h3>Gynecology</h3>
+                    <p>Comprehensive women's health services including reproductive care, prenatal, and postnatal support.</p>
+
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                    </div>
+                </div> 
+                
+                <div class="service-box">
+                        <i class="fas fa-lungs-virus"></i>
+                    <h3 class="">pulmonary</h3>
+                    <p>Specialized care for respiratory conditions such as asthma, COPD, and lung infections.</p>
+                    
+                    <div class="btn-readmore">
+                        <button class="readmore">Read More</button>
+                    </div>
+                </div> 
+
+            </div>
+        </div>
+
+</section>
+
+<section class="about-page">
+    <h1 class="tittle"><span>What Our </span> Patients Say!</h1>
     
 </section>
 
